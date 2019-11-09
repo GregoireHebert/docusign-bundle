f12aa58bbc8ed013931f55262a36f416bc9861de
Vincent Chalamon <vincentchalamon@gmail.com>
2019-11-09 19:16:45 +0100

    V5 - Revamp Extension classe to allow configuration per document

--- src/DocusignBundle.php
+++ src/DocusignBundle.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace DocusignBundle;
 
-use DocusignBundle\DependencyInjection\Compiler\EnvelopeBuilderPass;
 use DocusignBundle\DependencyInjection\Compiler\PluginPass;
-use DocusignBundle\DependencyInjection\Compiler\SignatureExtractorPass;
 use League\FlysystemBundle\FlysystemBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -15,9 +13,7 @@ final class DocusignBundle extends Bundle
 {
     public function build(ContainerBuilder $container): void
     {
-        $container->addCompilerPass(new EnvelopeBuilderPass());
         $container->addCompilerPass(new PluginPass());
-        $container->addCompilerPass(new SignatureExtractorPass());
 
         if (!class_exists(FlysystemBundle::class)) {
             $container->addCompilerPass(new PluginPass());
