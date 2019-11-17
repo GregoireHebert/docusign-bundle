4c91c5b91e6cb413850d75ea7f8d5e6ef7e0f013
Grégoire Hébert <gregoire@les-tilleuls.coop>
2019-11-17 14:34:17 +0100

    specify rights on file

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -10,3 +10,5 @@ mkdir $JWT_PATH
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$DOCUSIGN_RSA_PASSPHRASE" \
 --output $JWT_PATH/docusign.pem $SCRIPTS_PATH/docusign.pem.gpg
+
+chmod 644 $JWT_PATH/docusign.pem
