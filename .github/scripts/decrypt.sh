e757c92107df9b34198f4e912812cf4b80d3c351
Grégoire Hébert <gregoire@les-tilleuls.coop>
2019-11-17 11:07:20 +0100

    encode private key

---
+++ .github/scripts/decrypt.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+PROJECT_PATH=/__w/docusign-bundle/docusign-bundle
+JWT_PATH=$PROJECT_PATH/features/var/jwt
+SCRIPTS_PATH=$PROJECT_PATH/.github/scripts
+
+# Decrypt the file
+mkdir $JWT_PATH
+
+# --batch to prevent interactive command --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$DOCUSIGN_RSA_PASSPHRASE" \
+--output $JWT_PATH/docusign.pem $SCRIPTS_PATH/docusign.pem.gpg
