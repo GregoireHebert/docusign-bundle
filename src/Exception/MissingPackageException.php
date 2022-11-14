8196b9f80a4f6dbd0dfbc0cca3ba7ccf7793d00d
Vincent Chalamon <vincentchalamon@protonmail.com>
2022-11-14 15:32:43 +0100

    fix: CS

--- src/Exception/MissingPackageException.php
+++ src/Exception/MissingPackageException.php
@@ -13,14 +13,12 @@ declare(strict_types=1);
 
 namespace DocusignBundle\Exception;
 
-use Throwable;
-
 /**
  * flysystem compatibility.
  */
 class MissingPackageException extends \RuntimeException
 {
-    public function __construct($message = '', Throwable $previous = null)
+    public function __construct($message = '', \Throwable $previous = null)
     {
         parent::__construct($message, 0, $previous);
     }
